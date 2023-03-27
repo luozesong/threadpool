d3410e4954b43f53613c0624e0076722b7b4ff9a
小蜜蜂爱编程 <1311891520@qq.com>
2023-03-27 16:43:12 +0800

    add thread num function

--- usage.cpp
+++ usage.cpp
@@ -4,6 +4,20 @@
 void self_add(int& i) {
     ++i;
 }
+
+int get_threadnum(int job_num) {
+    uint32_t ncpu_num = get_nprocs();//the number of available processors
+    int nthread_count = 0;    //线程个数
+    if (job_num >= ncpu_num) {
+        nthread_count = ncpu_num; //多线程
+    } else if (job_num >= 2) {
+        nthread_count = job_num; //多线程
+    } else { 
+        nthread_count = 1; //单线程
+    }
+    return nthread_count;
+}
+
 int main() {
     int task_num = 10000;
     int n_threadnum = get_threadnum(task_num);
